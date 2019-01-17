317d0df0e39eb83eff22bd4e4ee396656f4fab8b
Patrick McMichael <patrick@patrickmcmichael.org>
2019-01-17 18:48:27 +0000

    Add GA code

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-36892073-3`,
       },
     },
     `gatsby-plugin-react-helmet`,
