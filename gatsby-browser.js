f62fd58e457dbec85aee4cb5cafdcb8ecbf9d841
Patrick McMichael <patrick@patrickmcmichael.org>
2019-01-19 22:17:50 +0000

    Remove typeface imports

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,2 @@
-// custom typefaces
-import 'typeface-montserrat'
-import 'typeface-merriweather'
 import './src/utils/atom-dark.css'
 // import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
