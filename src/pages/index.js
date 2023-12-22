b919ae9a3fa881ef3d02f1b2d1fa184ffd1a3064
Patrick McMichael <patrick@patrickmcmichael.org>
2023-12-22 15:42:42 +0000

    Move to layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,9 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
-import { defineCustomElements as deckDeckGoHighlightElement } from "@deckdeckgo/highlight-code/dist/loader";
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-
-deckDeckGoHighlightElement();
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
